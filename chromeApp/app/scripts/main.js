c6926b4b2395c5496f8c3da60de401c05c3d8f3a
Guy Sopher <guyso@wix.com>
2018-04-08 01:00:02 +0300

    serial read working with console + removed react

--- chromeApp/app/scripts/main.js
+++ chromeApp/app/scripts/main.js
@@ -2,7 +2,16 @@
 
 // Listens for the app launching then creates the window
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('../react/dist/index.html', {
-    state: 'fullscreen'
+  var width = 400;
+  var height = 300;
+
+  chrome.app.window.create('index.html', {
+    id: (new Date).toString(),
+    outerBounds: {
+      width: screen.availWidth - 100,
+      height: screen.availHeight - 100,
+      left: 50,
+      top: 50
+    }
   });
 });
