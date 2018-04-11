bc64478b7ce7b9fc69ba3c72edf676d33a87f6b0
Guy Sopher <guyso@wix.com>
2018-04-12 00:18:50 +0300

    arduino comm with chrome (both ways) working

--- staticArduino/zefishes-staticarduino.ino
+++ staticArduino/zefishes-staticarduino.ino
@@ -15,11 +15,60 @@ class Fans {
         }
         
         void relax() {
+            say("Fans off");
             digitalWrite(pin, LOW);
         }
         
         void blow() {
+            say("Fans on");
             digitalWrite(pin, HIGH);
+        }
+
+};
+
+//-----------------------------------------------------------------------------------//
+//-------------------------------------| LIGHTS |------------------------------------//
+//-----------------------------------------------------------------------------------//
+
+class Lights {
+    int pin1;
+    int pin2;
+    int pin3;
+
+    public:
+        Lights(int _pin1, int _pin2, int _pin3) {
+            pin1 = _pin1;
+            pin2 = _pin2;
+            pin3 = _pin3;
+        }
+
+        void setup() {
+          pinMode(pin1, OUTPUT);
+          pinMode(pin2, OUTPUT);
+          pinMode(pin3, OUTPUT);
+        }
+        
+        void on() {
+            say("Lights on");
+            digitalWrite(pin1, HIGH);
+            digitalWrite(pin2, HIGH);
+            digitalWrite(pin3, HIGH);
+        }
+        
+        void off() {
+            say("Lights off");
+            digitalWrite(pin1, LOW);
+            digitalWrite(pin2, LOW);
+            digitalWrite(pin3, LOW);
+        }
+        
+        void dance(int loops) {
+            String str = ("Dancing! ");
+            str += loops;
+            say(str);
+            digitalWrite(pin1, ((loops % 3) == 0));
+            digitalWrite(pin2, ((loops % 3) == 1));
+            digitalWrite(pin3, ((loops % 3) == 2));
         }
 
 };
@@ -51,21 +100,22 @@ class RF {
                 
         String receive() {
             if (rfSwitch.available()) {
-                // Serial.println(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),mySwitch.getReceivedProtocol());
-                // String str = "";
-                // str += "------------------| RECEIVING: " + rfSwitch.getReceivedValue();
-                // say(str);
                 rfSwitch.resetAvailable();
-                return rfSwitch.getReceivedValue();
+                return (String)rfSwitch.getReceivedValue();
             } else {
-                // String str = "";
-                // str += "...................";
-                // Serial.println(str);
                 return "";
             }
         }
                 
+        String mockReceive() {
+            if (random(0, 20) <= 1) {
+                say("Simulating Fish #2");
+                return ("![PLAY 2]");
+            } else {
+                return "";
+            }
 
+        }
 };
 
 //-----------------------------------------------------------------------------------//
@@ -74,6 +124,9 @@ class RF {
 
 class Music {
     public:
+    
+        String curStatus = "WAITING";
+        
         Music() {
         }
 
@@ -82,54 +135,102 @@ class Music {
         }
 
                 
-        void play(int set) {
-            // if (random(0, 1000) <= 1) {
-        
-                String str = "";
-                str += "![PLAY 2]";
-                Serial.println(str);
-        
-            // } else {
-            
-            //     if (rfSwitch.available()) {
-            //         // Serial.println(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),mySwitch.getReceivedProtocol());
-            //         String str = "";
-            //         str += "------------------| RECEIVING: " + rfSwitch.getReceivedValue();
-            //         say(str);
-            //         rfSwitch.resetAvailable();
-            //     } else {
-            //         String str = "";
-            //         str += "...................";
-            //         Serial.println(str);
-            //     }
-            // }
-            
+        void play(String set) {
+            String str = "![PLAY ";
+            str += set;
+            str += "]";
+            Serial.println(str);
         }
                 
+        void stop() {
+            String str = "![STOP]";
+            Serial.println(str);
+        }
+                
+        void idle() {
+            String str = "![IDLE]";
+            Serial.println(str);
+        }
+        
+        String setStatus() {
+            
+            String status = "";
 
+            Serial.setTimeout(50);
+            status = (String)Serial.readString();
+            say("Music status is: ");
+            say(status);
+            say((String)status.indexOf("PLAYING"));
+            say((String)status.indexOf("WAITING"));
+            if (status.indexOf("PLAYING") >= 0) {
+
+                curStatus = "PLAYING";
+                say("Current Status is: " + curStatus);
+            } else if (status.indexOf("WAITING") >= 0) {
+                curStatus = "WAITING";
+                say("Current Status is: " + curStatus);
+            }
+            return status;
+        }
+
+        bool isPlaying() {
+            this->setStatus();
+            const bool isPlaying = curStatus != "WAITING";
+            say("isPlaying: " + (String)isPlaying);
+            return isPlaying;
+        }
 };
 
 //-----------------------------------------------------------------------------------//
 //--------------------------------------| MAIN |-------------------------------------//
 //-----------------------------------------------------------------------------------//
 
-Fans fans = Fans(0); //D0 is the fans switch
-RF rf = RF(A0); //D0 is the fans switch
+RF      rf      = RF(A0); //A0 is the rf listener pin
+Fans    fans    = Fans(0); //D0 is the fans switch
+Music   music   = Music();
+Lights  lights  = Lights(1,2,3); //three lights on pins D2, D2, D3
+
+const int MAX_LOOP = 1*2*3*4*5*6*7*8*9*10;
+int loops = 0;
+
+String zeFish;
 
 void setup(){
-    fans.setup();
     rf.setup();
+    fans.setup();
+    music.setup();
+    lights.setup();
 }
 
 
 void loop() {
-
-    rf.receive();        
-    delay(250);
+    
+    if (music.isPlaying()) {
+        say("Music still playing...");
+        fans.blow();
+        lights.dance(loops);
+    } else {
+        zeFish = rf.mockReceive();
+        if (zeFish.length() > 0) {
+            say("Received fish: " + zeFish);
+            music.play(zeFish);
+            fans.blow();
+            lights.dance(loops);
+        } else {
+            say("No fishes...");
+            music.idle();
+            fans.relax();
+            lights.off();
+        }
+    }
+    
+    loops = (loops + 1) % MAX_LOOP;
+    delay(200);
 }
 
 void say(String toSay) {
-    Serial.println(toSay);
+    Serial.println(" <>===<><>---------->> " + toSay);
+    delay(10);
 }
 
 
