21def6c3b315c03c1aecfd98b0ff3f7213c998af
Guy Sopher <guyso@wix.com>
2018-04-11 21:23:06 +0300

    split to modules

--- staticArduino/zefishes-staticarduino.ino
+++ staticArduino/zefishes-staticarduino.ino
@@ -1,54 +1,195 @@
-// This #include statement was automatically added by the Particle IDE.
+//-----------------------------------------------------------------------------------//
+//--------------------------------------| FANS |-------------------------------------//
+//-----------------------------------------------------------------------------------//
+
+class Fans {
+    int pin;
+
+    public:
+        Fans(int fansPin) {
+            pin = fansPin;
+        }
+
+        void setup() {
+          pinMode(pin, OUTPUT);
+        }
+        
+        void relax() {
+            digitalWrite(pin, LOW);
+        }
+        
+        void blow() {
+            digitalWrite(pin, HIGH);
+        }
+
+};
+
+//-----------------------------------------------------------------------------------//
+//--------------------------------------| RF |-------------------------------------//
+//-----------------------------------------------------------------------------------//
 #include <RCSwitch.h>
 
-#define rfReceivePin A0  //RF Receiver pin = Analog pin 0
-#define ledPin 7        //Onboard LED = digital pin 13
+class RF {
+    int pin;
 
-unsigned int data = 0;   // variable used to store received data
-const unsigned int upperThreshold = 70;  //upper threshold value
-const unsigned int lowerThreshold = 50;  //lower threshold value
+    unsigned int data = 0;   // variable used to store received data
+    unsigned int loops = 0;
+    const unsigned int upperThreshold = 70;  //upper threshold value
+    const unsigned int lowerThreshold = 50;  //lower threshold value
+    
+    RCSwitch rfSwitch = RCSwitch();
+    
+    public:
+        RF(int rfPin) {
+            pin = rfPin;
+        }
 
-RCSwitch mySwitch = RCSwitch();
+        void setup() {
+            rfSwitch.enableReceive(pin);  // Receiver on interrupt 0 => that is pin #2
+        }
+
+                
+        String receive() {
+            if (rfSwitch.available()) {
+                // Serial.println(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),mySwitch.getReceivedProtocol());
+                // String str = "";
+                // str += "------------------| RECEIVING: " + rfSwitch.getReceivedValue();
+                // say(str);
+                rfSwitch.resetAvailable();
+                return rfSwitch.getReceivedValue();
+            } else {
+                // String str = "";
+                // str += "...................";
+                // Serial.println(str);
+                return "";
+            }
+        }
+                
+
+};
+
+//-----------------------------------------------------------------------------------//
+//--------------------------------------| MAIN |-------------------------------------//
+//-----------------------------------------------------------------------------------//
+
+class Music {
+    public:
+        Music() {
+        }
+
+        void setup() {
+
+        }
+
+                
+        void play(int set) {
+            // if (random(0, 1000) <= 1) {
+        
+                String str = "";
+                str += "![PLAY 2]";
+                Serial.println(str);
+        
+            // } else {
+            
+            //     if (rfSwitch.available()) {
+            //         // Serial.println(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),mySwitch.getReceivedProtocol());
+            //         String str = "";
+            //         str += "------------------| RECEIVING: " + rfSwitch.getReceivedValue();
+            //         say(str);
+            //         rfSwitch.resetAvailable();
+            //     } else {
+            //         String str = "";
+            //         str += "...................";
+            //         Serial.println(str);
+            //     }
+            // }
+            
+        }
+                
+
+};
+
+//-----------------------------------------------------------------------------------//
+//--------------------------------------| MAIN |-------------------------------------//
+//-----------------------------------------------------------------------------------//
+
+Fans fans = Fans(0); //D0 is the fans switch
+RF rf = RF(A0); //D0 is the fans switch
 
 void setup(){
-    
-    pinMode(ledPin, OUTPUT);
-    Serial.begin(9600);
-
-    mySwitch.enableReceive(A0);  // Receiver on interrupt 0 => that is pin #2
-    
+    fans.setup();
+    rf.setup();
 }
 
-unsigned int loops = 0;
 
 void loop() {
-    
-    if (mySwitch.available()) {
-        // Serial.println(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),mySwitch.getReceivedProtocol());
-        Serial.print(loops);
-        Serial.print(") ------------------| RECEIVING: ");
-        Serial.println(mySwitch.getReceivedValue());
-        mySwitch.resetAvailable();
-    } else {
-        Serial.print(loops);
-        Serial.println(") ...................");
-    }
+
+    rf.receive();        
     delay(250);
-    loops++;
-
-    // data = analogRead(rfReceivePin);    //listen for data on Analog pin 0
-   
-    // if (data > upperThreshold){
-    //     digitalWrite(ledPin, LOW);   //If a LOW signal is received, turn LED OFF
-    //     Serial.print("............ ");
-    //     Serial.println(data);
-    // }
-
-    // if (data < lowerThreshold){
-    //     digitalWrite(ledPin, HIGH);   //If a HIGH signal is received, turn LED ON
-    //     Serial.print("**** RECEIVING: ");
-    //     Serial.println(data);
-    // }
-
-    // delay(100);
 }
+
+void say(String toSay) {
+    Serial.println(toSay);
+}
+
+
+// //--------------------------------------| MAIN |-------------------------------------//
+// // This #include statement was automatically added by the Particle IDE.
+// #include <RCSwitch.h>
+
+// #define rfReceivePin A0  //RF Receiver pin = Analog pin 0
+// #define ledPin 7        //Onboard LED = digital pin 13
+
+// unsigned int data = 0;   // variable used to store received data
+// const unsigned int upperThreshold = 70;  //upper threshold value
+// const unsigned int lowerThreshold = 50;  //lower threshold value
+
+// RCSwitch mySwitch = RCSwitch();
+// Fans fans = Fans(0); //D0 is the fans switch
+
+// void setup(){
+    
+//     fans.setup();
+//     pinMode(ledPin, OUTPUT);
+//     Serial.begin(9600);
+
+//     mySwitch.enableReceive(A0);  // Receiver on interrupt 0 => that is pin #2
+    
+// }
+
+// unsigned int loops = 0;
+
+// void loop() {
+    
+//     if (random(0, 10) <= 1) {
+
+//         String str = "";
+//         str += loops;
+//         str += ") ![PLAY 5]";
+//         say(str);
+
+//     } else {
+    
+//         if (mySwitch.available()) {
+//             // Serial.println(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),mySwitch.getReceivedProtocol());
+//             String str = "";
+//             str += loops + ") ------------------| RECEIVING: " + mySwitch.getReceivedValue();
+//             say(str);
+//             mySwitch.resetAvailable();
+//         } else {
+//             String str = "";
+//             str += loops;
+//             str += ") ...................";
+//             say(str);
+//         }
+//     }
+    
+//     delay(250);
+//     loops++;
+// }
+
+// void say(String toSay) {
+//     Serial.println(toSay);
+    
+//     //Serial.println((String)'[|' + str + '|]');
+// }
